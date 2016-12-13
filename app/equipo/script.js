b364fa471105e853d9a2395d68b3a07c5d3e967c
edisonmora95 <edison_andre_9@hotmail.com>
2016-12-13 13:04:15 -0500

    Tercera modificacion funcion createDivs
    Esta vez fue por algo mio

--- app/equipo/script.js
+++ app/equipo/script.js
@@ -62,10 +62,6 @@ obj = {
 		},
 	]
 }
-
-
-
-
 $(document).ready(function(){
 
 	createDivs(obj)
@@ -74,49 +70,50 @@ $(document).ready(function(){
 function createDivs(objJSON){	
 	var clasePanel;	//nombre completo de la clase del panel
 	var panel, panelBody, imgPanel, pPanel;
-	
 	//PRIMERO CREAR EL DIV DE PROFESORES
 	var arrayProf = objJSON.profesores;
 	arrayProf.forEach(function(profesor){
-		clasePanel = 'panel panel-default panel-profesor';
-		panel = $('<div/>').addClass(clasePanel);
-		panelBody = $('<div/>').addClass('panel-body');
-		imgPanel = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt: 'imagen profesor'});
-		pPanel = $('<p/>').text(profesor.nombre);
-		panelBody.append(imgPanel, pPanel);
-		panel.append(panelBody);
-		$('.profesores').append(panel);
+		crearPanel('.profesores', 'profesor', profesor);
 	});
 	//DIVS DE AYUDANTES
 	var arrayAyudantes = objJSON.ayudantes;
 	arrayAyudantes.forEach(function(ayudante){
 		if(ayudante.tipo==='grader'){
-			clasePanel = 'panel panel-default panel-ayudante-deberes';
+			//DIV AYUDANTE DEBERES
+			/*clasePanel = 'panel panel-default panel-ayudante-deberes';
 			panel = $('<div/>').addClass(clasePanel);
 			panelBody = $('<div/>').addClass('panel-body');
 			imgPanel = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt: 'imagen ayudante de deberes'});
 			pPanel = $('<p/>').text(ayudante.nombre);
 			panelBody.append(imgPanel, pPanel);
 			panel.append(panelBody);
-			$('.ayudante-deberes').append(panel);
-		}else if(ayudante.tipo==='ta'){
-			clasePanel = 'panel panel-default panel-ayudante-clases';
+			$('.ayudante-deberes').append(panel);*/
+			crearPanel('.ayudante-deberes', 'ayudante-deberes', ayudante);
+		}else if(ayudante.tipo==='ta'){		
+		//DIV AYUDANTE CLASES
+			/*clasePanel = 'panel panel-default panel-ayudante-clases';
 			panel = $('<div/>').addClass(clasePanel);
 			panelBody = $('<div/>').addClass('panel-body');
 			imgPanel = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt: 'imagen ayudante de clases'});
 			pPanel = $('<p/>').text(ayudante.nombre);
 			panelBody.append(imgPanel, pPanel);
 			panel.append(panelBody);
-			$('.ayudante-clases').append(panel);
+			$('.ayudante-clases').append(panel);*/
+			crearPanel('.ayudante-clases', 'ayudante-clases', ayudante);
 		}else{
 			console.log('error en tipo de ayudante');
 		}
-
-
-		
 	});
+}
 
-	
-	
-	
+function crearPanel(contenedor, clase, obj){
+	clasePanel = 'panel panel-default panel-' + clase;
+	panel = $('<div/>').addClass(clasePanel);
+	panelBody = $('<div/>').addClass('panel-body');
+	var infoAlt = 'imagen ' + clase
+	imgPanel = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt: infoAlt});
+	pPanel = $('<p/>').text(obj.nombre);
+	panelBody.append(imgPanel, pPanel);
+	panel.append(panelBody);
+	$(contenedor).append(panel);
 }
