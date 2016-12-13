939b863516a69244c0af2398e8bb4f86df1475aa
edisonmora95 <edison_andre_9@hotmail.com>
2016-12-13 10:16:50 -0500

    Modificada funcion createDivs
    Modificada para que pueda crear divs con JSONs de ayudantes de clases y
    deberes. Falta profesores y coordinador

--- app/equipo/script.js
+++ app/equipo/script.js
@@ -1,31 +1,141 @@
-array = ['hola', 'mundo']
 
+/*
+ayudanteClases = {
+	matricula: 201304614,
+	nombre: Edison
+	correo: edanmora@espol.edu.ec,
+	horarioS1: 14h30-16h30,
+	aulaS1: 24A-103,
+	horarioS2: 10h30-12h30,
+	aulaS2: 24A-104,
+	horarioC1: 14h30-16h30,
+	aulaC1: 24A-103,
+	horarioC2: 10h30-12h30,
+	aulaC2: 24A-104,
+	profAsignado: Marco Antonio Calderon
+	paralelo: 1
+}*/
+var ayudantesDeberes = [
+	{
+		'matricula': '201304614',
+		'nombre': 'Edison',
+		'correo': 'edanmora@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '1'
+	},
+	{
+		'matricula': '2013123456',
+		'nombre': 'Xavier',
+		'correo': 'xidrovo@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '2'
+	},
+	{
+		'matricula': '201304614',
+		'nombre': 'Jaminson',
+		'correo': 'jamytafy@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '3'
+	}
+];
+
+
+var ayudantesClases = [
+	{
+		'matricula': '201304614',
+		'nombre': 'Edison',
+		'correo': 'edanmora@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '1', 
+		'horarioS1': '14h30-16h30',
+		'aulaS1': '24A-103',
+		'horarioS2': '10h30-12h30',
+		'aulaS2': '24A-104',
+		'horarioC1': '14h30-16h30',
+		'aulaC1': '24A-103',
+		'horarioC2': '10h30-12h30',
+		'aulaC2': '24A-104',
+	},
+	{
+		'matricula': '2013555555',
+		'nombre': 'Julio',
+		'correo': 'julyguilinder@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '2', 
+		'horarioS1': '14h30-16h30',
+		'aulaS1': '24A-103',
+		'horarioS2': '10h30-12h30',
+		'aulaS2': '24A-104',
+		'horarioC1': '14h30-16h30',
+		'aulaC1': '24A-103',
+		'horarioC2': '10h30-12h30',
+		'aulaC2': '24A-104',
+	},
+	{
+		'matricula': '2013444444',
+		'nombre': 'Joel',
+		'correo': 'joel@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '3', 
+		'horarioS1': '14h30-16h30',
+		'aulaS1': '24A-103',
+		'horarioS2': '10h30-12h30',
+		'aulaS2': '24A-104',
+		'horarioC1': '14h30-16h30',
+		'aulaC1': '24A-103',
+		'horarioC2': '10h30-12h30',
+		'aulaC2': '24A-104',
+	},
+	{
+		'matricula': '2013333333',
+		'nombre': 'Lenin',
+		'correo': 'lenin@espol.edu.ec',
+		'profAsignado': 'Marco Antonio Calderon',
+		'paralelo': '4', 
+		'horarioS1': '14h30-16h30',
+		'aulaS1': '24A-103',
+		'horarioS2': '10h30-12h30',
+		'aulaS2': '24A-104',
+		'horarioC1': '14h30-16h30',
+		'aulaC1': '24A-103',
+		'horarioC2': '10h30-12h30',
+		'aulaC2': '24A-104',
+	}
+
+];
+var array = ['hola', 'mundo']
 
 $(document).ready(function(){
-	console.log('hola');
-	createDivs('.profesores', array, 'profesor')
-	createDivs('.ayudante-clases', array, 'ayudante-clases')
-	createDivs('.ayudante-deberes', array, 'ayudante-deberes')
+	//console.log('hola');
+	//createDivs('.profesores', array, 'profesor')
+	createDivs('.ayudante-clases', ayudantesClases, 'ayudante-clases')
+	createDivs('.ayudante-deberes', ayudantesDeberes, 'ayudante-deberes')
 })
 
 
 function createDivs(contenedor, array, nombreClase){
-	
+	//contenedor -> el div en el cual se van a crear los divs
+	//array -> el array JSON
+	//nombreClase -> nombre de la clase para el nuevo panel a crear dentro del contenedor
 	
 	//array va a tener los objetos de profesores, ayudantes de deberes o ayudantes de clases
-	var obj;
-	var nombre, img;
-	for(obj in array){
-		//nombre = obj.nombre;	//Esto va a cambiar dependiendo de como sea el objeto que reciba
-		//img = obj.img;
-		var clase = 'panel panel-default panel-' + nombreClase
-		var panel = $('<div/>').addClass(clase)
-		var panelBody = $('<div/>').addClass('panel-body')
-		var imgProf = $('<img/>').addClass('img-responsive center-block').attr({src: '#', alt: 'profesor'})
-		var pProf = $('<p/>').text('Nombre')
-		panelBody.append(imgProf, pProf)
+	var clasePanel;	//nombre completo de la clase del panel
+	var panel, panelBody, imgPanel, pPanel;
+	array.forEach(function(obj){
+		//console.log(obj.nombre)
+		//console.log(obj.matricula)
+		clasePanel = 'panel panel-default panel-' + nombreClase
+		panel = $('<div/>').addClass(clasePanel)
+		panelBody = $('<div/>').addClass('panel-body-')
+		imgPanel = $('<img/>').addClass('img-responsive center-block').attr({src: '#', alt: nombreClase})
+		pPanel = $('<p/>').text(obj.nombre)
+		panelBody.append(imgPanel, pPanel)
 		panel.append(panelBody)
-		$(contenedor).append(panel);
-	}
+		$(contenedor).append(panel)
+	});
+
+
+
+	
 	
 }
