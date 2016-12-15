661f18e9cf233c3d4cb53a28a79879e7e634b7d3
edisonmora95 <edison_andre_9@hotmail.com>
2016-12-15 11:25:19 -0500

    Arreglado problema con Coordinador en equipo.html

--- app/equipo/script.js
+++ app/equipo/script.js
@@ -1,173 +1,16 @@
-obj = {
-	'graders' : [{
-			'matricula' : '7777777777',
-			'nombre' : 'grader1',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'profesor' : ['profesor1', 'profesor2'],
-			'paralelo' : ['paralelo1', 'paralelo2'],
-			'tipo' : 'grader' 
-		},
-		{
-			'matricula' : '8888888888',
-			'nombre' : 'grader2',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'profesor' : ['profesor1', 'profesor2'],
-			'paralelo' : ['paralelo1', 'paralelo2'],
-			'tipo' : 'grader' 
-		},
-		{
-			'matricula' : '9999999999',
-			'nombre' : 'grader3',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'profesor' : ['profesor1', 'profesor2'],
-			'paralelo' : ['paralelo1', 'paralelo2'],
-			'tipo' : 'grader'  
-		},
-		{
-			'matricula' : '0000000000',
-			'nombre' : 'grader4',
-			'correo' : ['correo1@espol.edu.ec', 'correo5@espol.edu.ec'],
-			'profesor' : ['profesorA', 'profesorB'],
-			'paralelo' : ['paraleloA', 'paraleloB'],
-			'tipo' : 'grader'  
-		}
-	],
-	'TA' : [
-		{
-			'matricula' : '1111111111',
-			'nombre' : 'ayudanteTA1',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula1', 'aula2'],
-			'tipo' : 'ta' 
-		},
-		{
-			'matricula' : '2222222222',
-			'nombre' : 'ayudanteTA2',
-			'correo' : 'correo',
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula3', 'aula4'],
-			'tipo' : 'ta'  
-		},
-		{
-			'matricula' : '3333333333',
-			'nombre' : 'ayudanteTA3',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula5', 'aula6'],
-			'tipo' : 'ta'  
-		},
-		{
-			'matricula' : '4444444444',
-			'nombre' : 'ayudanteTA4',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula7', 'aula8'],
-			'tipo' : 'ta'  
-		},
-		{
-			'matricula' : '5555555555',
-			'nombre' : 'ayudanteTA5',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula9', 'aula10'],
-			'tipo' : 'ta'  
-		},
-		{
-			'matricula' : '6666666666',
-			'nombre' : 'ayudanteTA6',
-			'correo' : ['correo1@espol.edu.ec', 'correo2@espol.edu.ec'],
-			'horarios' : ['horario1', 'horario2'],
-			'aula' : ['aula11', 'aula12'],
-			'tipo' : 'ta'  
-		},
-	],
-	'profesores' : [
-		{
-	      "nombre": "AVENDAÑO SUDARIO ALLAN ROBERTO",
-	      "aula": [
-	        "A-210",
-	        "24C-205 FIMCP",
-	        "COMP A FIMCP"
-	      ],
-	      "paralelo": [
-	        "37",
-	        "34"
-	      ],
-	      "dia": [
-	        "LUNES",
-	        "MI�RCOLES",
-	        "JUEVES",
-	        "MARTES"
-	      ],
-	      "hora": [
-	        "7:30:00-9:30:00",
-	        "15:30:00-17:30:00"
-	      ],
-	      "correo": [
-	        "aavendan@fiec.espol.edu.ec",
-	        "aavendan@espol.edu.ec"
-	      ],
-	      "img": "https://www.fiec.espol.edu.ec/sites/fiec.espol.edu.ec/files/javiles.jpg"
-	    },
-		{
-	      "nombre": "BONILLA RAFAEL IGNACIO",
-	      "aula": [
-	        "15A-05",
-	        "A-208",
-	        "LPU1",
-	        "LPU2"
-	      ],
-	      "paralelo": [
-	        "8",
-	        "7"
-	      ],
-	      "dia": [
-	        "JUEVES",
-	        "MARTES"
-	      ],
-	      "hora": [
-	        "14:30:00-16:30:00",
-	        "11:30:00-13:30:00"
-	      ],
-	      "correo": [
-	        "rabonilla@espol.edu.ec"
-	      ],
-	      "img": "https://www.fiec.espol.edu.ec/sites/fiec.espol.edu.ec/files/rafael%20bonilla.jpg"
-	    },
-		{
-	      "nombre": "CALDERON ARGUELLO MARCO ANTONIO",
-	      "aula": [
-	        "IC-27",
-	        "IL02 - LABORATORIO BETA"
-	      ],
-	      "paralelo": [
-	        "4"
-	      ],
-	      "dia": [
-	        "JUEVES",
-	        "MARTES"
-	      ],
-	      "hora": [
-	        "14:30:00-16:30:00"
-	      ],
-	      "correo": [
-	        "ancalder@espol.edu.ec"
-	      ],
-	      "img": "https://www.fiec.espol.edu.ec/sites/fiec.espol.edu.ec/files/mcalderon.jpg"
-	    }
-	]
-}
-$(document).ready(function(){
-	//console.log(info);
-	createDivs(info);
-	crearModal();
 
+$(document).ready(function(){
+	crearModal();
+	createDivs(info);
 })
 
 function createDivs(objJSON){	
 	var clasePanel;	//nombre completo de la clase del panel
 	var panel, panelBody, imgPanel, pPanel;
+	//DIV DE COORDINADOR
+	crearPanel('.coordinador', 'coordinador', objJSON.profesores[1]);
+
+
 	//DIV DE PROFESORES
 	var arrayProf = objJSON.profesores;
 	arrayProf.forEach(function(profesor){
@@ -199,18 +42,17 @@ function crearPanel(contenedor, clase, obj){
 		'data-toggle' : 'modal',
 		'data-target' : '#myModal'
 	});
-
+	//Para editar la informacion que se muestra al abrir el modal
 	panel.click(function(){
 		if(obj.tipo==='grader'){
-			editarModalGrader(obj.nombre, obj.correo, obj.profesor, obj.paralelo, obj.tipo);
-		}else if(obj.tipo==='ta'){
-			editarModalTA(obj.nombre, obj.correo, obj.horarios, obj.aula, obj.tipo);
+			editarModalAyudantes(obj.nombre,'http://placehold.it/350x150','', obj.correo, obj.profesor, obj.paralelo, obj.tipo);
+		}else if(obj.tipo==='TA'){
+			editarModalAyudantes(obj.nombre,'http://placehold.it/350x150', obj.correo, null, obj.horario, obj.aula, obj.tipo);
 		}else{
 			editarModalProfesor(obj.nombre, obj.correo, obj.aula, obj.paralelo, obj.dia, obj.hora, obj.img);
 		}
 	});
 	$(contenedor).append(panel);
-	
 }
 
 function crearModal(){
@@ -237,34 +79,25 @@ function crearModal(){
 	$('body').append(modal);
 }
 
-function editarModalGrader(name, arrayMail, arrayProfesor, arrayParalelo, tipo){
-	$('.modal-body').empty();
-	var img = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt : 'imagen'});
+function editarModalAyudantes(name, imgSrc, mail, arrayMail, array1, array2, tipo){
+	$('.modal-body').empty()	//Vacio el popup
+	var img = $('<img/>').addClass('img-responsive center-block').attr({src: imgSrc, alt : 'imagen'});
 	var nombre = $('<p/>').text(name);
 	$('.modal-body').append(img, nombre);
-	arrayMail.forEach(function(email){
-				$('.modal-body').append($('<p/>').text(email))
-	});
-	for(var i = 0 ; i < arrayProfesor.length ; i++){
-				var texto = arrayProfesor[i] + " : " + arrayParalelo[i];
+	//Para anadir la informacion de los mails
+	if(mail!=''){	//Solo si es TA se le pasa algo en mail
+		$('.modal-body').append($('<p/>').text(mail));
+	}else{		//Si no es TA entonces mail esta vacio y se usa arrayMail
+		arrayMail.forEach(function(email){
+			$('.modal-body').append($('<p/>').text(email))
+		});
+	}
+	//Para anadir informacion de horarios, paralelos, profesores y aulas
+	for(var i = 0 ; i < array1.length ; i++){
+				var texto = array1[i] + " : " + array2[i];
 				$('.modal-body').append($('<p>').text(texto));
 	}
-	$('.modal-header').empty();
-	var header = $('<h4/>').text(tipo);
-	$('.modal-header').append(header);
-}
-
-function editarModalTA(name, mail, arrayHorarios, arrayAulas, tipo){
-	$('.modal-body').empty();
-	var img = $('<img/>').addClass('img-responsive center-block').attr({src: 'http://placehold.it/350x150', alt : 'imagen'});
-	var nombre = $('<p/>').text(name);
-	$('.modal-body').append(img, nombre);
-	$('.modal-body').append($('<p/>').text(mail))
-	
-	for(var i = 0 ; i < arrayHorarios.length ; i++){
-				var texto = arrayHorarios[i] + " : " + arrayAulas[i];
-				$('.modal-body').append($('<p>').text(texto));
-	}
+	//Cambiar el header
 	$('.modal-header').empty();
 	var header = $('<h4/>').text(tipo);
 	$('.modal-header').append(header);
@@ -286,4 +119,3 @@ function editarModalProfesor(name, arrayMail, arrayAula, arrayParalelo, arrayDia
 	var header = $('<h4/>').text('Profesor');
 	$('.modal-header').append(header);
 }
-
