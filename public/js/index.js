d07aa7a14ff630540ef3f084a9921ad17b33e31e
Xidrovo <xidrovo@hotmail.com>
2016-12-18 23:23:16 -0500

    Creado, html para teachers
    faltaría crear para Curso, y Equipo
    
    Entrar como Yo
    user: user
    pass: user
    
    Entrar como profesor
    user: admin
    pass: admin

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,20 @@ $("#begin").click(function() {
         scrollTop: $("#title-1").offset().top
     }, 1000);
 });
+
+$("#entrar").click(function(){
+	if($("#usr").val() == "user" && $("#pwd").val() == "user")
+	{
+		window.location.href = "indexStudent.html";
+	}else if($("#usr").val() == "admin" && $("#pwd").val() == "admin")
+	{
+		window.location.href = "indexTeacher.html";
+	}else
+	{
+		alert("usuario o contraseña equivocada");
+	}
+});
+
+$("#sign-out").click(function(){
+		window.location.href = "index.html";
+})
