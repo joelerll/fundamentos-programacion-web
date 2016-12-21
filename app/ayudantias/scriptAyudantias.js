2d1e0e0c18a7591e518082b7fbb8f330821df596
edisonmora95 <edison_andre_9@hotmail.com>
2016-12-21 09:36:05 -0500

    El Proyecto 3: Revenge of the Map
    El mapa ya es responsive...

--- app/ayudantias/scriptAyudantias.js
+++ app/ayudantias/scriptAyudantias.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 	createDivs(info);
-	$('#mapModal').on('show.bs.modal', function(){
-		setTimeout(function() {
-		    map.invalidateSize();
-		}, 10);
-	});
 
 });
 
@@ -21,16 +16,15 @@ function createDivs(JSON){
 function createRow(obj, container, clase, i, idMapa){
 	var row = $('<div/>').addClass('row featurette');
 	var colImg, colInfo, colMap;
-	colImg = $('<div/>').addClass('col-md-4');
-	colInfo = $('<div/>').addClass('col-md-4');
-	colMap = $('<div/>').addClass('col-md-4').attr({'id' : idMapa});
+	colImg = $('<div/>').addClass('col-md-4 col-xs-12 col-sm-12');
+	colInfo = $('<div/>').addClass('col-md-4 col-xs-12 col-sm-12');
+	colMap = $('<div/>').addClass('col-md-4 col-xs-12 col-sm-12').attr({'id' : idMapa});
 
 	var img = $('<img/>').addClass('img-responsive center-block').attr({'src': obj.img, 'alt': 'imagen de ta', 'id' : 'imgAyudante'});
 	colImg.append(img);
-
 	var nombre = $('<p/>').text(obj.nombre);
 	var correo = $('<p/>').text(obj.correo);
-	colInfo.append(nombre, correo);
+	colInfo.append( $('<h4/>').text('Nombre'), nombre,  $('<h4/>').text('Correo'), correo, $('<h4/>').text('Horarios'));
 
 	var horario;
 	for (var i = 0; i < obj.horario.length; i++) {
@@ -59,55 +53,7 @@ function createRow(obj, container, clase, i, idMapa){
 			marker = L.marker([lat, lng]).addTo(map);
 			aula = obj.aula[j];
 			marker.bindPopup(aula).openPopup();
-		}
-
-	/*
-	var row = $('<div/>').addClass('row featurette');
-	var colImg, colInfo;
-	if(i%2==0){
-		colImg = $('<div/>').addClass('col-md-4');
-		colInfo = $('<div/>').addClass('col-md-8');
-	}else{
-		colImg = $('<div/>').addClass('col-md-4 col-md-push-8');
-		colInfo = $('<div/>').addClass('col-md-8 col-md-pull-4');
-	}
-	img = $('<img/>').addClass('img-responsive center-block').attr({'src': obj.img, 'alt': 'imagen de ta', 'id' : 'imgAyudante'});
-	colImg.append(img);
-	
-	var nombre = $('<p/>').text(obj.nombre);
-	var correo = $('<p/>').text(obj.correo);
-	colInfo.append(nombre, correo);
-	
-	var horario;
-	for (var i = 0; i < obj.horario.length; i++) {
-		horario = obj.horario[i] + ' en ' + obj.aula[i];
-		colInfo.append($('<p/>').text(horario));
-	}
-	var mapLink = $('<p/>').attr({
-		'id' : 'mapLink',
-		'data-toggle' : 'modal',
-		'data-target' : '#mapModal'
-	}).text('Ver mapa');
-
-	mapLink.click(function(){
-		//crearModal();
-		//crearMapa();
-		map.setView(new L.LatLng(obj.lat[0], obj.lng[0]), 15);
-		var marker, lat, lng, aula;
-		for (var j = 0; j < obj.lat.length; j++) {
-			lat = obj.lat[j];
-			lng = obj.lng[j];
-			marker = L.marker([lat, lng]).addTo(map);
-			aula = obj.aula[j];
-			marker.bindPopup(aula).openPopup();
-		}
-	});
-
-
-	colInfo.append(mapLink);*/
-	
-
-	
+		}	
 }
 
 function crearModal(){
@@ -126,15 +72,4 @@ function crearModal(){
 	modal.append(modalDialog);
 	
 	$('body').append(modal);
-}
-
-
-function crearMapa(){
-	var map = L.map('map',{
-		    center: [43.64701, -79.39425],
-		    zoom: 15
-	});
-
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
-
 }
